e68cc0e9764d468a1fc573898ac76d10456698b5
pinghow0220 <leepinghow0220@gmail.com>
2024-11-19 15:59:27 +0800

    19/11 v1

--- renobridge/urls.py
+++ renobridge/urls.py
@@ -6,25 +6,25 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('renobridge/', views.renobridge, name='renobridge'),  # For index.html
-    path('dashboard/', views.dashboard, name='dashboard'),  # For dashboard.html
+    path('renobridge/', views.renobridge, name='renobridge'),  
+    path('dashboard/', views.dashboard, name='dashboard'), 
     path('tips01/', views.tips01, name='tips01'),
     path('tips02/', views.tips02, name='tips02'),
     path('tips03/', views.tips03, name='tips03'),
     path('about_us/', views.about_us, name='about_us'),
     path('view_homeowner_input/', views.view_homeowner_input, name='view_homeowner_input'),
     path('edit_homeowner_input/', views.edit_homeowner_input, name='edit_homeowner_input'),
-    path('expert_list/', views.expert_list, name='expert_list'),  # For expert_list.html
-    path('expert_portfolio/<int:id>', views.expert_portfolio, name='expert_portfolio'),  # For expert_portfolio.html
-    path('expert_profile/<int:id>/', views.expert_profile, name='expert_profile'),  # For expert_profile.html
+    path('expert_list/', views.expert_list, name='expert_list'),  
+    path('expert_portfolio/<int:id>', views.expert_portfolio, name='expert_portfolio'), 
+    path('expert_profile/<int:id>/', views.expert_profile, name='expert_profile'), 
     path('edit_contractor_profile/<int:id>/', views.edit_contractor_profile, name='edit_contractor_profile'),
-    path('experts_input/', views.experts_input, name='experts_input'),  # For experts_input.html
+    path('experts_input/', views.experts_input, name='experts_input'),  
     path('completion_page/<int:project_id>/', views.completion_page, name='completion_page'),
    
-    path('myfirst/', views.myfirst, name='myfirst'),  # For myfirst.html
-    path('owner_confirmation_list/', views.owner_confirmation_list, name='owner_confirmation_list'),  # For owner_confirmation_list.html
-    path('owner_input/', views.owner_input, name='owner_input'),  # For owner_input.html
-    path('register/', views.register, name='register'),  # For register.html
+    path('myfirst/', views.myfirst, name='myfirst'),  
+    path('owner_confirmation_list/', views.owner_confirmation_list, name='owner_confirmation_list'),  
+    path('owner_input/', views.owner_input, name='owner_input'),  
+    path('register/', views.register, name='register'),  
     path('expert_invitation_list/',views.expert_invitation_list, name='expert_invitation_list'),
     path('expert_dashboard/', views.expert_dashboard, name='expert_dashboard'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
@@ -44,6 +44,8 @@ urlpatterns = [
     path('project/<int:project_id>/expenses/', views.view_expenses, name='view_expenses'),
     path('download_invoice/<int:project_id>/', views.download_invoice, name='download_invoice'),
     path('submit_review/<int:project_id>/', views.submit_review, name='submit_review'),
+    path('project/<int:project_id>/expense/<int:expense_id>/delete/', views.delete_expense, name='delete_expense'),
+    path('project/<int:project_id>/details/', views.project_details, name='project_details'),
 ]
 
 if settings.DEBUG:
